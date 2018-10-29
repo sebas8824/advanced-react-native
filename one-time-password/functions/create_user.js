45356ab9d35c2070004e859047cd42e7b0cebe85
sebas8824 <sebastian.benitez@outlook.com>
2018-10-28 21:37:16 -0500

    Included the request body verification and phone number structure to persist it into firebase datastore

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,3 +1,21 @@
+const admin = require('firebase-admin');
+
 module.exports = (request, response) => {
-    response.send(request.body);
+    // Verify the user provided a Phone
+    if (!request.body.phone) {
+        response.status(422).send({ error: 'Bad input' });
+    }
+    
+    // Format the phone number, to remove dashes and parens
+    const phone = String(request.body.phone)
+    // TODO: Build a function to treat the phone input and control its http response.
+        .replace(/(?:\+\d+)?(?:\s+)(?:(\d+)-)?(\d+)/, "$1$2")
+        .replace(/[^\d]/g, "");
+    
+    // Create a new user account using that phone number
+    admin.auth().createUser({ uid: phone })
+        .then(user => response.send(user))
+        .catch(err => response.status(422).send({ error: err }));
+    
+    // Respond to the user request, saying the account was made.  
 }
